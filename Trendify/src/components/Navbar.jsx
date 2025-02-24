136e91dd941a2362e74fe25eb4f2e9f592f9ef98
divyanshuthakur14 <singhthakurdivyanshu@gmail.com>
2025-02-24 23:55:04 +0530

    Initial commit

---
+++ Trendify/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import {assets} from '../assets/assets'
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between py-5 font-medium'>
+      <img src={assets.logo} className='w-36' alt="" />
+    </div>
+  )
+}
+
+export default Navbar
