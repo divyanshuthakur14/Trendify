11cc35d7b274b9787f0f008c01e2b5318a39dbb9
divyanshuthakur14 <singhthakurdivyanshu@gmail.com>
2025-03-07 21:58:11 +0530

    next commit

---
+++ Trendify/src/components/ProductItem.jsx
@@ -0,0 +1,26 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
+
+const ProductItem = ({ id, image, name, price }) => {
+  const { currency } = useContext(ShopContext);
+
+  return (
+    <Link className="text-gray-700 cursor-pointer" to={`/product/${id}`}>
+      <div>
+        <img
+          className="hover:scale-110 transition ease-in-out"
+          src={Array.isArray(image) ? image[0] : image} // ✅ Handles both array and string
+          alt={name}
+        />
+      </div>
+      <p className="pt-3 pb-1 text-sm">{name}</p>
+      <p className="text-sm font-medium">
+        {currency}
+        {price}
+      </p>
+    </Link>
+  );
+};
+
+export default ProductItem;
--- Trendify/src/pages/Home.jsx
+++ Trendify/src/pages/Home.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-
+import Hero from '../components/Hero'
+import LatestCollection from '../components/LatestCollection'
 const Home = () => {
   return (
     <div>
-      
+      <Hero />
+      <LatestCollection/>
     </div>
   )
 }
